5ca3ba87ebe48be26b92962313ccb0ebe92314f2
Maxence <119626292+vitagerman@users.noreply.github.com>
2024-04-25 20:08:02 +0200

    Musique qui se repète à la fin du niveau test fix

--- Assets/Scripts/Core/BackgroundMusic.cs
+++ Assets/Scripts/Core/BackgroundMusic.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 public class BackgroundMusic : MonoBehaviour
 {
     public AudioClip backgroundMusic; // La musique de fond à jouer
+    private static bool musicStarted = false; // Variable statique pour suivre si la musique a déjà commencé
 
     void Awake()
     {
         // Assurez-vous que cet objet persiste entre les scènes
         DontDestroyOnLoad(gameObject);
 
-        // Configurez l'AudioSource pour jouer la musique de fond
-        AudioSource audioSource = GetComponent<AudioSource>();
-        audioSource.loop = true;
-        audioSource.clip = backgroundMusic;
+        // Vérifie si la musique n'a pas déjà commencé
+        if (!musicStarted)
+        {
+            // Configurez l'AudioSource pour jouer la musique de fond
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.loop = true;
+            audioSource.clip = backgroundMusic;
 
-        // Jouez la musique de fond
-        audioSource.Play();
+            // Jouez la musique de fond
+            audioSource.Play();
+
+            // Indique que la musique a commencé
+            musicStarted = true;
+        }
     }
 }
