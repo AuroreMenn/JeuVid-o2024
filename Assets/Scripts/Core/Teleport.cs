c5d6d218e62fc7e170aa9b9c59eedd4c5971c91f
Maxence <119626292+vitagerman@users.noreply.github.com>
2024-04-25 12:55:19 +0200

    Lier les scenes entre elle

--- Assets/Scripts/Core/Teleport.cs
+++ Assets/Scripts/Core/Teleport.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Teleport : MonoBehaviour
 {
 	public float x; //Rentrer ici la coordonnée x où vous souhaitez téléporter le joueur
 	public float y; //Rentrer ici la coordonnée y où vous souhaitez téléporter le joueur
 	public int numTableau; //Rentrer ici l'indice du tableau que vous voulez désormais afficher
+	public string sceneName;
 	
 	public void SetPosition(float _x, float _y){
 		x = _x;
@@ -21,13 +24,15 @@ public class Teleport : MonoBehaviour
 		//Si le téléporteur entre en collision avec le joueur (objet avec le tag "Player")
         if (col.gameObject.tag == "Player") {
 			//On modifie la position de l'objet touché (le joueur) et on le téléporte à x et y
-			col.gameObject.GetComponent<PlayerManagerAnimated>().Teleport(x,y,numTableau); //On récupère le PlayerManager du joueur pour appeler la fonction de téléportation
+			//col.gameObject.GetComponent<PlayerManagerAnimated>().Teleport(x,y,numTableau); //On récupère le PlayerManager du joueur pour appeler la fonction de téléportation
 			
 			//On fait apparaître le nouveau tableau selon le numéro entré (numTableau).
-			TableauManager.ShowTableau(numTableau);
+			//TableauManager.ShowTableau(numTableau);
 			
 			//Les nouvelles coordonnées du checkpoint sont celles où le joueur vient d'être téléporté
-			TableauManager.UpdateCheckpointPosition(x,y);
+			//TableauManager.UpdateCheckpointPosition(x,y);
+
+			SceneManager.LoadScene(sceneName);
         }
     }
 }
